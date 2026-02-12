106ad8a8939f24f0afea53a5c1efc5c923679df2
hacker-is-undefeatable <williamstudymaterials@gmail.com>
2026-02-12 11:30:12 +0800

    remove frontend

--- frontend/src/lib/wagmi.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { base, mainnet, optimism } from 'wagmi/chains';
-
-export const config = getDefaultConfig({
-  appName: 'PumpPilot',
-  projectId: 'YOUR_WALLETCONNECT_PROJECT_ID',
-  chains: [base, mainnet, optimism],
-  ssr: true, // If your dApp uses server side rendering (SSR)
-});
